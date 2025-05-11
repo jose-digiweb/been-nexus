d51030f9c6475243f0d7e39e75a4d95c818000ca
jose-digiweb <josefurtado.digital@gmail.com>
2025-05-11 20:44:09 +0200

    nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,7 @@ export default defineNuxtConfig({
     isEnabled: true,
     disableServerSideAuth: false,
     originEnvKey: 'AUTH_ORIGIN',
+    baseURL: 'http://localhost:3000/api/auth',
     sessionRefresh: {
       enablePeriodically: true,
       enableOnWindowFocus: true,
