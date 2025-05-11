d52a40653ea1d51f6243a077d65cc0f7ba3a99ed
jose-digiweb <josefurtado.digital@gmail.com>
2025-05-11 11:59:20 +0200

    Integrated cloudflare workers and done first deploy manually using wrangler cli

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  nitro: {
+    preset: 'cloudflare-module',
+
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true,
+    },
+  },
+
+  modules: ['nitro-cloudflare-dev'],
+});
