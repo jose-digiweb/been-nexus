a3b5b40bef3a5bc208c3e6996240edac9e4a504d
jose-digiweb <josefurtado.digital@gmail.com>
2025-05-14 19:12:03 +0200

    Updated kinde envs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,4 +23,14 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxtjs/kinde',
   ],
+
+  kinde: {
+    authDomain: process.env.NUXT_KINDE_AUTH_DOMAIN,
+    clientId: process.env.NUXT_KINDE_CLIENT_ID,
+    clientSecret: process.env.NUXT_KINDE_CLIENT_SECRET,
+    redirectURL: process.env.NUXT_KINDE_REDIRECT_URL,
+    logoutRedirectURL: process.env.NUXT_KINDE_LOGOUT_REDIRECT_URL,
+    postLoginRedirectURL: process.env.NUXT_KINDE_POST_LOGIN_REDIRECT_URL,
+    password: process.env.NUXT_KINDE_PASSWORD,
+  },
 });
