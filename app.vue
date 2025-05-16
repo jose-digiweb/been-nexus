fa0f80db7c446dd6471f6205fa1a4b68fad96734
jose-digiweb <josefurtado.digital@gmail.com>
2025-05-16 11:14:56 +0200

    Added clerk

--- app.vue
+++ app.vue
@@ -1,26 +1,12 @@
-<script lang="ts" setup>
-const { loggedIn, user } = useAuth();
-
-console.log({
-  loggedIn,
-  user,
-});
-</script>
-
 <template>
   <UApp>
-    <div class="flex items-center justify-between p-4">
-      <h1>Been Nexus</h1>
-
-      <div class="flex items-center">
-        <div v-if="loggedIn && user" class="flex items-center gap-2">
-          <h2>Welcome back, {{ user.given_name }}</h2>
-          <UIcon name="lucide-minus" />
-          <NuxtLink to="/api/logout" external>Sign out</NuxtLink>
-        </div>
-
-        <NuxtLink v-else to="/api/login" external>Sign in</NuxtLink>
-      </div>
-    </div>
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
   </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,30 +17,5 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: [
-    'nitro-cloudflare-dev',
-    '@nuxt/eslint',
-    '@nuxt/ui',
-    '@nuxtjs/kinde',
-  ],
-
-  kinde: {
-    authDomain: process.env.NUXT_KINDE_AUTH_DOMAIN,
-    clientId: process.env.NUXT_KINDE_CLIENT_ID,
-    clientSecret: process.env.NUXT_KINDE_CLIENT_SECRET,
-    redirectURL: process.env.NUXT_KINDE_REDIRECT_URL,
-    logoutRedirectURL: process.env.NUXT_KINDE_LOGOUT_REDIRECT_URL,
-    postLoginRedirectURL: process.env.NUXT_KINDE_POST_LOGIN_REDIRECT_URL,
-    password: process.env.NUXT_KINDE_PASSWORD,
-  },
-
-  runtimeConfig: {
-    authDomain: process.env.NUXT_KINDE_AUTH_DOMAIN,
-    clientId: process.env.NUXT_KINDE_CLIENT_ID,
-    clientSecret: process.env.NUXT_KINDE_CLIENT_SECRET,
-    redirectURL: process.env.NUXT_KINDE_REDIRECT_URL,
-    logoutRedirectURL: process.env.NUXT_KINDE_LOGOUT_REDIRECT_URL,
-    postLoginRedirectURL: process.env.NUXT_KINDE_POST_LOGIN_REDIRECT_URL,
-    password: process.env.NUXT_KINDE_PASSWORD,
-  },
+  modules: ['nitro-cloudflare-dev', '@nuxt/eslint', '@nuxt/ui', '@clerk/nuxt'],
 });
