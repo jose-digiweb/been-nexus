cbf147c998df460336709576d0c274832678f11a
jose-digiweb <josefurtado.digital@gmail.com>
2025-05-16 11:35:09 +0200

    Some cleanup and Welcome Clerk

--- app.vue
+++ app.vue
@@ -1,8 +1,15 @@
 <template>
   <UApp>
-    <header>
+    <header class="flex items-center justify-between p-4 sm:p-6">
+      <NuxtLink to="/" class="uppercase font-bold text-lg mask-x-from-90%"
+        >Been Nexus</NuxtLink
+      >
+
       <SignedOut>
-        <SignInButton />
+        <SignInButton
+          mode="modal"
+          class="cursor-pointer hover:bg-primary/90 px-4 py-1 rounded-md bg-primary dark:text-slate-950"
+        />
       </SignedOut>
       <SignedIn>
         <UserButton />
