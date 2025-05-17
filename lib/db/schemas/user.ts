d87a2bb4952dd8cff17d5c036872ec958f6de63d
jose-digiweb <josefurtado.digital@gmail.com>
2025-05-17 16:49:37 +0200

    wip

--- lib/db/schemas/user.ts
+++ lib/db/schemas/user.ts
@@ -1,6 +1,6 @@
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const user = sqliteTable('user', {
+export const place = sqliteTable('place', {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
   email: text().notNull().unique(),
